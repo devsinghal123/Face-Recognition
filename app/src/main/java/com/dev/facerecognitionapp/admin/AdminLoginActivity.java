e00cf8ebad0d26abb29ddcf38c1be8a319d6b6b3
devsinghal123 <singhaldev1975@gmail.com>
2023-07-14 13:53:18 +0530

    i have added admin login activity

---
+++ app/src/main/java/com/dev/facerecognitionapp/admin/AdminLoginActivity.java
@@ -0,0 +1,16 @@
+package com.dev.facerecognitionapp.admin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.dev.facerecognitionapp.R;
+
+public class AdminLoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_admin_login);
+    }
+}
